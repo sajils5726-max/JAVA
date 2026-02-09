415812df8635f2d02bcf13af7cdf26c52e632028
SaJiL S <sajilsajeev5726@gmail.com>
2026-02-09 19:06:22 +0530

    Create Data.java

---
+++ Data.java
@@ -0,0 +1,12 @@
+public class Data 
+{
+    void getData(int a)
+    {
+        System.out.println("A="+a);
+    }
+    public static void main(String[] args) 
+    {
+        Data ob=new Data();
+        ob.getData(100);
+    }
+}
