2c1b9e75eceff0843875ea904d4b7181d25bbb33
SaJiL S <sajilsajeev5726@gmail.com>
2026-02-09 19:34:16 +0530

    Create StudentDemo.java

---
+++ StudentDemo.java
@@ -0,0 +1,15 @@
+class Student
+{
+    void msg()
+    {
+       System.out.println("Sample case");
+    }
+}
+class StudentDemo
+{
+    public static void main(String args[])
+    {
+        Student std = new Student();
+        std.msg();
+    }
+}
