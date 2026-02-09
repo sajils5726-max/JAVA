8b835a5dcc46fac88d090eee72ff8660200e7f80
SaJiL S <sajilsajeev5726@gmail.com>
2026-02-09 19:02:49 +0530

    Create ObjDemo.java

---
+++ ObjDemo.java
@@ -0,0 +1,12 @@
+public class ObjDemo 
+{
+    void msg()
+    {
+        System.out.println("Class & Object");
+    }
+    public static void main(String[] args)
+    {
+        objDemo ob=new objDemo();
+        ob.msg();
+    }
+}
