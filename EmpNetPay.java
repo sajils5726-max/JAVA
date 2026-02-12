1a8aa17799df1cf262575f7a40bd7fbc1ee92b2b
SaJiL S <sajilsajeev5726@gmail.com>
2026-02-12 14:56:13 +0530

    Correct syntax errors in EmpNetPay.java
    Fixed syntax errors and improved variable declarations in the EmpNetPay class.

--- EmpNetPay.java
+++ EmpNetPay.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 public class EmpNetPay
 {
-    int empid, TA, DA, HRA, T ax, netsal, basic;
+    int empid, TA, DA, HRA, Tax, netsal, basic;
     String empname;
     void getdata()
     {
         Scanner scan = new Scanner(System.in);
-        System. out println("Enter Employee name, id, Basic Pay, TA, DA, HRA and Tax");
-        empname = scan.nextLine();
-        empid = scan.nextlnt();
-        basic=scan.nextlnt():
-        TA = scan.nextlnt();
-        DA = scan.nextint();
-        HRA = scan.nextlnt();
-        Tax = scan.nextint();
+        System.out.println("Enter Employee name, id, Basic Pay, TA, DA, HRA and Tax");
+        empname=scan.nextLine();
+        empid=scan.nextInt();
+        basic=scan.nextInt();
+        TA=scan.nextInt();
+        DA=scan.nextInt();
+        HRA=scan.nextInt();
+        Tax=scan.nextInt();
     }
     void calc()
     {
-        netsal=(basiC+TA+DA+HRA)-Tax
-        System.out println("Net Salary:"+netsal);
+        netsal=(basic+TA+DA+HRA)-Tax;
+        System.out.println("Net Salary:"+netsal);
     }
     public static void main( String args[])
     {
-        EmpNetPay e1 = new EmpNetPay0;e1.getdata0;
+        EmpNetPay e1 = new EmpNetPay();
+        e1.getdata();
         System.out.println("InEmployee Details");
-        System. out println("-----------------");
-        System.out, println("Employee ID: "+e1.empid);
-        System.out.printn("Employee name:"+e1.empname);
+        System. out.println("-----------------");
+        System.out.println("Employee ID: "+e1.empid);
+        System.out.println("Employee name:"+e1.empname);
         e1.calc();
     }
 }
