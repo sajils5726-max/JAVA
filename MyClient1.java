637b70bcc66b995ab848315bedba98fe3972fd90
SaJiL S <sajilsajeev5726@gmail.com>
2026-04-02 22:24:50 +0530

    Refactor MyClient1 for code clarity and formatting

--- MyClient1.java
+++ MyClient1.java
@@ -1,15 +1,22 @@
 import java.io.*;
 import java.net.*;
-public class MyClient1 
+
+public class MyClient1
 {
-    public static void main(String[] args)throws IOException
+    public static void main(String[] args) throws IOException
     {
-        DatagramSocket dSocket=new DatagramSocket();
-        InetAddress add=InetAddress.getByName("localhost:");
-        String str="*Message to Server from Client*";
-        byte[]bufBytes=str.getBytes();
-        DatagramPacket datagramPacket=new DatagramPacket(bufBytes,bufBytes.length,add,9000); 
+        DatagramSocket dSocket = new DatagramSocket();
+
+        InetAddress add = InetAddress.getByName("localhost");
+
+        String str = "* Message to Server from Client *";
+
+        byte[] bufBytes = str.getBytes();
+
+        DatagramPacket datagramPacket =new DatagramPacket(bufBytes, bufBytes.length, add, 9000);
+
         dSocket.send(datagramPacket);
+
         dSocket.close();
     }
 }
