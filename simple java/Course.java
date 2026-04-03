26a1ea1188ed2e18bbb04bbd6ce759d01112f0f4
SaJiL S <sajilsajeev5726@gmail.com>
2026-04-03 09:14:51 +0530

    Create Course.java

---
+++ simple java/Course.java
@@ -0,0 +1,10 @@
+public class Course 
+{
+    public static void main(String[] args) 
+    {
+        int rollno=10;
+        String course="MCA";
+        System.out.println("Rollno :"+rollno);
+        System.out.println("Course :"+course);
+    }
+}
