a62f0cafafdea41f0bac846a8fab9c4d17967a5a
SaJiL S <sajilsajeev5726@gmail.com>
2026-04-03 09:18:11 +0530

    Delete RandNum.java

--- RandNum.java
+++
@@ -1,9 +0,0 @@
-import java.lang.Math;
-public class RandNum
-{
-    public static void main(String args[])
-    {
-    System.out.println("1st Random Number:"+Math.random());
-    System.out.println("2nd Random Number:"+Math.random());
-    }
-}
