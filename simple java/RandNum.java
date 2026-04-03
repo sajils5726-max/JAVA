6e73868c204001db20ca8d2ff5417e47c6335c66
SaJiL S <sajilsajeev5726@gmail.com>
2026-04-03 09:14:09 +0530

    Add files via upload

---
+++ simple java/RandNum.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+public class RandNum
+{
+    public static void main(String args[])
+    {
+    System.out.println("1st Random Number:"+Math.random());
+    System.out.println("2nd Random Number:"+Math.random());
+    }
+}
