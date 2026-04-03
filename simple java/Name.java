bf9ca7aa8de3cc67efc5147dc9b7b943e3e64da6
SaJiL S <sajilsajeev5726@gmail.com>
2026-04-03 09:17:29 +0530

    Add Name class with main method for output

---
+++ simple java/Name.java
@@ -0,0 +1,10 @@
+public class Name
+{
+    public static void main(String[] args)
+    {
+        int age=10;
+        String name="Akhil";
+        System.out.println("Name :"+name);
+        System.out.println("Age :"+age);
+    }
+}
